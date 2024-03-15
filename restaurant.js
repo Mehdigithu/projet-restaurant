1ba2fea6fcbaff64c0960a027f4e4e1095d6f007
jerou <jerou.mouheb94@gmail.com>
2024-03-15 16:43:19 +0100

    update mouheb

--- restaurant.js
+++ restaurant.js
@@ -7,3 +7,19 @@ taille.forEach(taille => {
 taille.addEventListener('mouseenter', function (event){taille.style.background='orange'})
 taille.addEventListener('mouseleave', function (event){taille.style.background='transparent'})
 });
+
+
+
+function filterNumbers(event) {
+    let saisie = event.target.value;// saisie = a
+    
+
+    saisie= saisie.replace(/[^0-9]/g, ''); // saisie=a.replace par ''=('') 
+    // saisie=5
+
+    event.target.value = saisie;// champs de saisie =''
+   // event.target.value =5
+}
+let reserver=document.querySelector('#reserver')
+reserver.addEventListener('mouseenter',function(event){reserver.style.color='black'})
+reserver.addEventListener('mouseleave',function(event){reserver.style.color='white'})
